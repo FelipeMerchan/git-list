eef277fb6d904a1b46b1266baba6e6acffc4b74e
Felipe Merchan <felipe-sc@hotmail.es>
2022-11-28 00:32:47 -0500

    12. 5 formas de añadir CSS en React.js

--- src/user.js
+++ src/user.js
@@ -1,4 +1,12 @@
-import { useState } from "react"
+import { useState } from 'react'
+import styled from 'styled-components'
+
+import style from './user.module.css'
+import './user.scss'
+
+const Avatar = styled.img`
+    border: 10px solid black;
+`
 
 export const User = ({ counter, name, avatar }) => {
     const [calculatedName, setCalculatedName] = useState(name)
@@ -8,9 +16,13 @@ export const User = ({ counter, name, avatar }) => {
     }
 
     return (
-        <div className='User' onClick={onClick}>
-            <img src={avatar} alt='' />
-            <p>Hemos dado clic en {calculatedName} por {counter} veces</p>
+        <div style={{
+            borderBottom: '10px solid red',
+            borderLeft: '5px solid blue',
+        }} className='User' onClick={onClick}>
+            <img className={style.avatar} src={avatar} alt={name} title={name} />
+            <Avatar src={avatar} alt={name} title={name} />
+            <p className='name'>Hemos dado clic en {calculatedName} por {counter} veces</p>
         </div>
     )
 }
