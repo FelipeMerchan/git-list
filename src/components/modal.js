41bc561ecc3382bb6cb30facdc6aef4ed67c4bab
Felipe Merchan <felipe-sc@hotmail.es>
2022-12-28 23:47:49 -0500

    31. Componentes basados en clases

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,11 +1,49 @@
-import { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { ButtonContrast } from './button'
 import InputText from './input-text'
-
 import Overlay from './overlay'
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.name = 'asdasd'
+  }
+  state = {
+    a: 'initial state for a'
+  }
+
+  componentDidUpdate() {
+    console.log('El componente se actualizo')
+  }
+
+  //Cuando el componente se va a desmontar
+  componentWillUnmount() {
+    console.log('El componente esta apunto de desaparecer')
+    debugger
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        a: 'Felipe',
+        b: this.name,
+      })
+    }, 3000)
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.a}
+        {this.state.b}
+        Modal clase
+      </div>
+    )
+  }
+}
 
 const ModalContentStyled = styled.form`
   position: fixed;
@@ -30,9 +68,11 @@ const ModalContentStyled = styled.form`
 function ModalContent() {
   const form = useRef(null)
   const navigator = useNavigate()
+  const [isActive, setIsActive] = useState(true)
 
   function handleSubmit(event) {
     event.preventDefault()
+    setIsActive(false)
     const formData = new FormData(form.current)
     navigator(`/${formData.get('username')}`)
   }
@@ -40,6 +80,10 @@ function ModalContent() {
 
   return (
     <Overlay>
+      {
+        isActive ? <Modal /> : null
+      }
+      <Modal />
       <ModalContentStyled ref={form} onSubmit={handleSubmit}>
         <h2 className='title'>Busca a tu usuario favorito</h2>
         <InputText type='text' autoComplete='off' name='username' placeholder='Username' />
